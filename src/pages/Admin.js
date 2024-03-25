c035a01f2f59cb11e43db701e46e693cc62e027a
hamdisaqaf <hamdisaqqaf@gmail.com>
2024-03-25 16:09:01 +0300

    get getBuyerRequest from app to admin page
    get getBuyerRequest from app to admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -11,8 +11,20 @@ import Badge from 'react-bootstrap/Badge';
 const AdminPage = () => {
   //let history = useHistory();
   const navigate = useNavigate();
+  const getData = async() => {
+    await axios.get('http://api.imagemarketing.net/getBuyerRequest',
+   ).then((data)=>{
+    setBuyerRequests(data.data)
+   })
+   }
+ 
+   useEffect(async()=> {
+     await getData()
+  }, [])
 
  // const [username, setUsername] = useLocalStorage("name", "");
+ let [buyerRequests, setBuyerRequests] = useState([])
+
   const [login, setLogin] = useLocalStorage("login", "false");
   console.log('login getLogin:', login)
   /*
@@ -67,6 +79,115 @@ const AdminPage = () => {
             CreateTire
           </Button>
         </Stack>;
+        <Container fluid="md" className="container">      
+      <Stack  gap={1} className="container mx-auto">
+      <Form.Text muted className="text-center">
+      <h4>
+      أهلاً بك في شركة الحجاجي للتجارة العامة - أجتكو 
+      </h4>
+      <h4>اختر مقاس اطار سيارتك من القائمة ادناه لتحصل على تفاصيل أكثر 
+      </h4>
+      </Form.Text>
+      <div>
+    <h1></h1>
+    </div>
+    </Stack>
+
+      <Container>
+        <Row>
+               {
+                 buyerRequests.map((val)=>{
+                    
+                  //const url ='https://web.whatsapp.com/send?phone=967775955150&text='
+                  const url ='https://wa.me/967775955150?text='+' من فضلك احتاج شراء اطار ماركة' +val.buyer_id+ ' المقاس:'+ val.buyer_id 
+                  + 'والمسعر بقيمة:'+ val.price  + 'دولار'
+                  return(
+                  <Col sm={4} xs="auto">
+                 
+                    <Badge bg="warning" text="dark">
+                    {val.buyer_id }
+                  
+                     </Badge>
+                <Card  key={val.id } bg="primary" sm={4}>
+                <Card.Header>
+                <Nav variant="pills" defaultActiveKey="#first">
+                    <Nav.Item>
+                      <Nav.Link  href="#link" onClick={()=>(navigate(`/tire/${val.id}`))}>مواصفات</Nav.Link>
+                    </Nav.Item>
+                  
+                    <Nav.Item>
+                      <Nav.Link href="#disabled" disabled>
+                        Disabled
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+             </Card.Header>
+
+                  <Card.Img variant="top" src={val.itemNo}  />
+                  <Card.Body>
+                    <Card.Title><Badge bg="warning" text="dark">
+                اطارات    {val.itemNo }
+                     </Badge>
+                     <h1 className="post-title" onClick={()=>(navigate(`/tire/${val.itemNo}`))}>{val.itemNo}</h1>       
+                     </Card.Title>
+                    <Card.Text>
+                   
+                    <h3>
+                     <Badge bg="secondary">{val.itemNo} </Badge> دولار
+                </h3>
+                    </Card.Text>
+                  </Card.Body>
+                  <ListGroup variant="flush" bg="danger">
+                 
+                  <ListGroup.Item>
+                  <div className="vr" />
+                  <Button variant="outline-danger" onClick={yemeniRials}>اضغط هنا للسعر بالريال</Button>
+                    {DolarexchangeRial} سعر الصرف
+                   <h5>
+                ريال <Badge bg="secondary">{productrialprice = (DolarexchangeRial * val.price)} </Badge> 
+                </h5></ListGroup.Item>
+                <div> </div> 
+                </ListGroup>
+                <div> </div> 
+                <Button as="a" variant="danger" size="lg" href={url} target='_blank'>
+                       شراء
+                    </Button>
+                <Table striped bordered hover variant="dark" responsive>
+                    <thead>
+                   
+                      <tr>
+                        <th>اقصى حمولة</th>
+                        <th> اقصى سرعة</th>
+                        <th>عمق الدعسة</th>
+                        <th>تصنيف التدحرج</th>
+                        <th>تصنيف القبضة الرطبة</th>
+                        <th>تصنيف الضجيج </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{val.itemNo}</td>
+                        <td>{val.itemNo}</td>
+                        <td>{val.itemNo}</td>
+                        <td>{val.product}</td>
+                        <td>{val.quantity}</td>
+                        <td>{val.quantity}</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Card>
+               </Col>            
+      
+                   )
+                })              
+               }
+               </Row>
+           </Container>            
+            
+               
+
+</Container>
+
         <Badge bg="danger" as={Button} onClick={
           () => click()
           } >{login ? "Logout" : "Login"} </Badge >
